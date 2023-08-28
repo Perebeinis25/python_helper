640f89364ccadc6efebdcebb0f9077b32a383332
Perebeinis25 <yraperebeynis6@gmail.com>
2023-08-28 16:48:52 +0300

    hw_6.Dictionary Changes 1.2

--- home_work/hw_6.py
+++ home_work/hw_6.py
@@ -3,8 +3,9 @@ def count_symbols(str_arg):
     result = {}
 
     for char in str_arg:
-        key = str_arg.count(char)
-        result[char] = key
+        if char not in result:
+            count_char = str_arg.count(char)
+            result[char] = count_char
 
     return result
 
@@ -18,36 +19,39 @@ def count_words(input_str):
     split_word = input_str.split()
 
     for word in split_word:
-        key = split_word.count(word)
-        result[word] = key
+        if word not in result:
+            count_word = split_word.count(word)
+            result[word] = count_word
 
     return result
 
 
-input_str = "Hello hello hellO world y x"
+input_str = "Hello hello hello hellO world y x"
 result = count_words(input_str)
 print("# Task_4: ", result)
 
 
 # Task_5
 def dict_union(dict_1, dict_2):
-    union = dict_1 | dict_2
+
     set_keys_dict_1 = set(dict_1)
     set_keys_dict_2 = set(dict_2)
 
-    total_len = len(dict_1) + len(dict_2)
-
-    list_value = []
-
-    list_value.extend(dict_1.values())
-    list_value.extend(dict_2.values())
     check = set_keys_dict_1.isdisjoint(set_keys_dict_2)
 
     if not check:
+        total_len = len(dict_1) + len(dict_2)
+
+        list_value = []
+
+        list_value.extend(dict_1.values())
+        list_value.extend(dict_2.values())
+
         var = dict(zip((range(0, total_len)), list_value))
         return var
 
     else:
+        union = dict_1 | dict_2
         return union
 
 
@@ -59,24 +63,29 @@ print("# Task_5: ", result)
 
 # Task_6
 def dict_union(dict_1, dict_2):
-    set_keys_dict_1 = set(dict_1)
-    set_keys_dict_2 = set(dict_2)
+
     key = 0
-    intersect = set_keys_dict_1.intersection(set_keys_dict_2)
+
+    result = {}
+    result.update(dict_1)
+    print(type(result), id(result), type(dict_1), id(dict_1),type(dict_2), id(dict_2))
 
     for j in dict_2:
+
+        set_keys_dict_1 = set(dict_1)
+        set_keys_dict_2 = set(dict_2)
+        intersect = set_keys_dict_1.intersection(set_keys_dict_2)
+
         if j in intersect:
 
             while key in dict_1 or key in dict_2:
                 key += 1
-            dict_1[key] = dict_2[j]
+            result[key] = dict_2[j]
             key += 1
 
         else:
-            dict_1[j] = dict_2[j]
-
-    else:
-        return dict_1
+            result[j] = dict_2[j]
+    return result
 
 
 dict_1 = {105: "Hello", 2: "Yra", 53: "Python", 5: "is", 10: "the"}
